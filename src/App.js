e9a9c2160165aabbc006f7b81105bb95eff760d6
beck-teck <love.guns.roses@gmail.com>
2023-06-25 14:42:36 +0900

    APIからデータを取得するようにした

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import {useEffect, useState } from 'react'  // reactライブラリ使用宣言
 import logo from './logo.svg';
 import './App.css';
 
+const url = 'https://weys9m11gc.execute-api.ap-northeast-1.amazonaws.com/Prod/hello'
+
 function App() {
+  const [messege, setMessage] = useState('loading...')
+  console.log(messege)
+
+  useEffect(() => {
+    fetch(url)                  // APIコール関数
+      .then(async value => {          // JavaScriptの非同期の処理
+        const json = await value.json()
+        setMessage(json.message)
+      })
+      .catch(reason => {
+        console.error(reason)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {messege}
         </p>
         <a
           className="App-link"
